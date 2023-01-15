a8615fc3a2bdea79a8848c4a77416fff2442eb5c
Len4ka <evr1503@mail.ru>
2023-01-15 20:54:00 +0500

    Добавлены функция и критерий поиска по номеру телефона в формате +()-

--- CsvAnalyst.py
+++ CsvAnalyst.py
@@ -108,7 +108,31 @@ def list_meet_email(fields_list):
     if ratio > 0.2:
         return True, ratio
     # Не набралось нужного количества совпадений
-    return False, ratio    
+    return False, ratio
+    
+# Если в этом поле номер телефона в формате +()-, то True    
+def meet_phone_number(field):
+    checkfor = ['+()-']
+    for s in checkfor:
+        if s in str(field): # Есть совпадения
+            return True
+    # Нет совпадений
+    return False
+    
+# Если в этом списке многие элементы содержат номер телефона в формате +()-, то True    
+def list_meet_phone_number(fields_list):
+    counter_total = 0
+    counter_meet = 0
+    for list_item in fields_list:
+        counter_total += 1
+        if meet_phone_number(list_item):
+            counter_meet += 1
+    # Конец подсчета
+    ratio = counter_meet / counter_total
+    if ratio > 0.5:
+        return True, ratio
+    # Не набралось нужного количества совпадений
+    return False, ratio        
     
 # Если в этом поле дата в формате AM/PM, то True    
 def meet_date_am_pm(field):
@@ -151,14 +175,21 @@ def check_all_columns(df):
         result2 = list_meet_email(lst)
         if result2[0]:
             output_text.insert(tk.END, "В столбце " + str(i+1) + " предположительно содержится email." + os.linesep)
-            output_text.insert(tk.END, "Процент совпадений " + "{:.2f}".format(result1[1]*100) + "%." + os.linesep + os.linesep)
+            output_text.insert(tk.END, "Процент совпадений " + "{:.2f}".format(result2[1]*100) + "%." + os.linesep + os.linesep)
             continue # Все нашли, можно идти к следующему столбцу
             
         # Третий критерий
-        result3 = list_meet_date_am_pm(lst)
+        result3 = list_meet_phone_number(lst)
         if result3[0]:
-            output_text.insert(tk.END, "В столбце " + str(i+1) + " предположительно содержится дата в формате AM/PM." + os.linesep)
+            output_text.insert(tk.END, "В столбце " + str(i+1) + " предположительно содержится номер телефона в формате +()-." + os.linesep)
             output_text.insert(tk.END, "Процент совпадений " + "{:.2f}".format(result3[1]*100) + "%." + os.linesep + os.linesep)
+            continue # Все нашли, можно идти к следующему столбцу    
+            
+        # Четвертый критерий
+        result4 = list_meet_date_am_pm(lst)
+        if result4[0]:
+            output_text.insert(tk.END, "В столбце " + str(i+1) + " предположительно содержится дата в формате AM/PM." + os.linesep)
+            output_text.insert(tk.END, "Процент совпадений " + "{:.2f}".format(result4[1]*100) + "%." + os.linesep + os.linesep)
             continue # Все нашли, можно идти к следующему столбцу
             
         # Соответствия критериям не найдено
